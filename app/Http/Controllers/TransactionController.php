60fe474a044cd39734ac4e18a4a658bf5146c5a5
sabrina <sabrinaizzahramadhani@gmail.com>
2026-09-10 17:23:45 +0700

    tambah halaman riwayat transaksi

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -55,7 +55,7 @@ class TransactionController extends Controller
 
     public function index()
     {
-        return 'Daftar transaksi';
+        return view('transactions.index');
     }
 
     public function show(string $id)
---
+++ resources/views/transactions/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'Riwayat Transaksi')
+
+@section('content')
+
+<h1 class="text-lg font-semibold mb-4">
+    Riwayat Transaksi
+</h1>
+
+<div class="border rounded-md p-4">
+    <p class="text-slate-500">
+        Belum ada transaksi.
+    </p>
+</div>
+
+@endsection
